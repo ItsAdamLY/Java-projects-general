cbe07d0359bfc94eaf6106700c7e9ab963788b37
ItsAdamLY <84637112+ItsAdamLY@users.noreply.github.com>
2021-05-23 01:36:07 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,5 +4,28 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        char status = 'p';
+        int service_year = 5;
+        double salary = 200.00;
+
+        if (status == 'F' || status == 'f'){
+            if (service_year < 5){
+                System.out.println("Your new salary is " + salary*1.04); // for Java they don't have data conversion thingy, just do " " + dataType(expression)
+            }
+            else {
+                System.out.println("Your new salary is " + salary*1.05);
+            }
+        }
+        else if (status == 'P' || status == 'p'){
+            if (service_year < 5){
+                System.out.println("Your new salary is " + salary*1.025);
+            }
+            else {
+                System.out.println("Your new salary is " + salary*1.03);
+            }
+        }
+        else{
+            System.out.println("Error! Wrong status.");
+        }
     }
-}
+ }
